b2a22ad4c3b073995a3c2767e2472cb2f15db86b
mrdylanfroment <mr.dylan.froment@gmail.com>
2025-08-22 19:36:58 +0100

    Create PartD-Python.py

---
+++ PartD-Python.py
@@ -0,0 +1,132 @@
+# -*- coding: utf-8 -*-
+#classes should go in pdtube.py
+import FreeCAD, FreeCADGui, Part
+class PDTube:
+    def __init__(self,obj):
+        obj.addProperty("App::PropertyLength","Radius1","Tube","Radius1").Radius1 = 5
+        obj.addProperty("App::PropertyLength","Radius2","Tube","Radius2").Radius2 = 10
+        obj.addProperty("App::PropertyLength","Height","Tube","Height of tube").Height = 10
+        self.makeAttachable(obj)
+        obj.Proxy = self
+
+    def makeAttachable(self, obj):
+
+        if int(FreeCAD.Version()[1]) >= 19:
+            obj.addExtension('Part::AttachExtensionPython')
+        else:
+            obj.addExtension('Part::AttachExtensionPython', obj)
+
+        obj.setEditorMode('Placement', 0) #non-readonly non-hidden
+
+    def execute(self,fp):
+        outer_cylinder = Part.makeCylinder(fp.Radius2, fp.Height)
+        inner_cylinder = Part.makeCylinder(fp.Radius1, fp.Height)
+        if fp.Radius1 == fp.Radius2: #just make cylinder
+            tube_shape = outer_cylinder
+        elif fp.Radius1 < fp.Radius2:
+            tube_shape = outer_cylinder.cut(inner_cylinder)
+        else: #invert rather than error out
+            tube_shape = inner_cylinder.cut(outer_cylinder)
+
+        if not hasattr(fp, "positionBySupport"):
+            self.makeAttachable(fp)
+        fp.positionBySupport()
+        tube_shape.Placement = fp.Placement
+
+        #BaseFeature (shape property of type Part::PropertyPartShape) is provided for us
+        #with the PartDesign::FeaturePython and related classes, but it might be empty
+        #if our object is the first object in the tree.  it's a good idea to check
+        #for its existence in case we want to make type Part::FeaturePython, which won't have it
+
+        if hasattr(fp, "BaseFeature") and fp.BaseFeature != None:
+            if "Subtractive" in fp.TypeId:
+                full_shape = fp.BaseFeature.Shape.cut(tube_shape)
+            else:
+                full_shape = fp.BaseFeature.Shape.fuse(tube_shape)
+            full_shape.transformShape(fp.Placement.inverse().toMatrix(), True) #borrowed from gears workbench
+            fp.Shape = full_shape
+        else:
+            fp.Shape = tube_shape
+        if hasattr(fp,"AddSubShape"): #PartDesign::FeatureAdditivePython and
+                                      #PartDesign::FeatureSubtractivePython have this
+                                      #property but PartDesign::FeaturePython does not
+                                      #It is the shape used for copying in pattern features
+                                      #for example in making a polar pattern
+            tube_shape.transformShape(fp.Placement.inverse().toMatrix(), True)
+            fp.AddSubShape = tube_shape
+
+class PDTubeVP:
+    def __init__(self, obj):
+        '''Set this object to the proxy object of the actual view provider'''
+        obj.Proxy = self
+
+    def attach(self,vobj):
+        self.vobj = vobj
+
+    def updateData(self, fp, prop):
+        '''If a property of the handled feature has changed we have the chance to handle this here'''
+        pass
+
+    def getDisplayModes(self,obj):
+        '''Return a list of display modes.'''
+        modes=[]
+        modes.append("Flat Lines")
+        modes.append("Shaded")
+        modes.append("Wireframe")
+        return modes
+
+    def getDefaultDisplayMode(self):
+        '''Return the name of the default display mode. It must be defined in getDisplayModes.'''
+        return "Flat Lines"
+
+    def setDisplayMode(self,mode):
+        '''Map the display mode defined in attach with those defined in getDisplayModes.\
+                Since they have the same names nothing needs to be done. This method is optional'''
+        return mode
+
+    def onChanged(self, vp, prop):
+        '''Here we can do something when a single property got changed'''
+        #FreeCAD.Console.PrintMessage("Change property: " + str(prop) + "\n")
+        pass
+
+    def getIcon(self):
+        '''Return the icon in XPM format which will appear in the tree view. This method is\
+                optional and if not defined a default icon is shown.'''
+        return """
+            /* XPM */
+            static const char * ViewProviderBox_xpm[] = {
+            "16 16 6 1",
+            "   c None",
+            ".  c #141010",
+            "+  c #615BD2",
+            "@  c #C39D55",
+            "#  c #000000",
+            "$  c #57C355",
+            "        ........",
+            "   ......++..+..",
+            "   .@@@@.++..++.",
+            "   .@@@@.++..++.",
+            "   .@@  .++++++.",
+            "  ..@@  .++..++.",
+            "###@@@@ .++..++.",
+            "##$.@@$#.++++++.",
+            "#$#$.$$$........",
+            "#$$#######      ",
+            "#$$#$$$$$#      ",
+            "#$$#$$$$$#      ",
+            "#$$#$$$$$#      ",
+            " #$#$$$$$#      ",
+            "  ##$$$$$#      ",
+            "   #######      "};
+            """
+
+    def dumps(self):
+        '''When saving the document this object gets stored using Python's json module.\
+                Since we have some un-serializable parts here -- the Coin stuff -- we must define this method\
+                to return a tuple of all serializable objects or None.'''
+        return None
+
+    def loads(self,state):
+        '''When restoring the serialized object from document we have the chance to set some internals here.\
+                Since no data were serialized nothing needs to be done here.'''
+        return None
